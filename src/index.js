e01ee5314d0b8b650f1af0bafe7efd5c592707b2
Alejandro <AlejandroFCasas@gmail.com>
2020-05-28 21:14:25 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 const path = require ('path')
 
 
-app.set ('port', 3000);
+app.set ('port', 5000);
 app.set('views', path.join(__dirname,'views'));
 app.engine ('html',require('ejs').renderFile);//a todos los html renderizarlos en ejs
 app.set ('view engine', 'ejs');
@@ -21,3 +21,17 @@ app.use(express.static(path.join(__dirname,'public')));
 app.listen(app.get('port'), () => {
     console.log('server:', app.get ('port'));
 });
+*/
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+
+app.get('/', (req,res) => {
+    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
+})
+
+const port = process.env.PORT || '5000';
+app.listen(port, () => console.log(`Server started on Port ${port}`));
